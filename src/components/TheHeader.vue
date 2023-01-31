ac29043fff5d84d8782e16fad98c3002562839fb
Luiz H. Celeguim <luiz.celeguim@gmail.com>
2023-01-31 21:00:27 +0100

    types

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,11 +1,18 @@
 <script lang="ts" setup>
 import UseEmojis2 from "@/composables/UseEmojis2";
+import { defineComponent } from "vue";
 const { findEmoji2 } = UseEmojis2();
+</script>
+
+<script lang="ts">
+export default defineComponent({
+  components: {},
+});
 </script>
 
 <template>
   <h1 class="text-6xl flex items-center">
-    <component width="40" :is="findEmoji2('file-notes')"></component>
+    <component width="60" :is="findEmoji2('file-notes')"></component>
     <strong>&nbsp; My Journal</strong>
   </h1>
 </template>
