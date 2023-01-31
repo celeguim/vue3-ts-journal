155a6c10444e500ab8b12110833a46490233f057
Luiz H. Celeguim <luiz.celeguim@gmail.com>
2023-01-31 21:50:45 +0100

    event function

--- src/components/EntryEditor.vue
+++ src/components/EntryEditor.vue
@@ -2,12 +2,22 @@
 import EmojiField from "./EmojiField.vue";
 import { defineComponent } from "vue";
 import UseEmojis2 from "@/composables/UseEmojis2";
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import type { Ref } from "vue";
 import type Emoji from "@/types/Emoji";
 const { findEmoji2 } = UseEmojis2();
 const text = ref("");
 const emoji: Ref<Emoji | null> = ref(null);
+const charCount = computed<number>(() => text.value.length);
+const maxChars = 280;
+const handleTextInput = (e: Event) => {
+  const textarea = e.target as HTMLTextAreaElement;
+  if (textarea.value.length <= maxChars) {
+    text.value = textarea.value;
+  } else {
+    text.value = textarea.value = textarea.value.substring(0, maxChars);
+  }
+};
 </script>
 
 <script lang="ts">
@@ -19,12 +29,13 @@ export default defineComponent({
 <template>
   <form class="entry-form" @submit.prevent>
     <textarea
-      v-model="text"
+      :value="text"
+      @keyup="handleTextInput"
       placeholder="New Journal Entry for danielkelly_io"
     ></textarea>
     <EmojiField v-model="emoji" />
     <div class="entry-form-footer">
-      <span>0 / 280</span>
+      <span>{{ charCount }} / {{ maxChars }}</span>
       <button>
         Remember
         <component
